6a5db1487061653347f5d5a5b888ad0d5b9c9489
hotianbexuanto <1772720865@qq.com>
2025-03-05 00:11:56 +0800

    1.21.1

--- .github/devbuilds/get_number.js
+++ .github/devbuilds/get_number.js
@@ -1,3 +1,12 @@
+import fetch from 'node-fetch';
+
 fetch("https://meteorclient.com/api/stats")
     .then(res => res.json())
-    .then(res => console.log("number=" + (parseInt(res.devBuild) + 1)))
+    .then(res => {
+        const nextBuildNumber = (parseInt(res.devBuild) + 1) || 1;
+        console.log("number=" + nextBuildNumber);
+    })
+    .catch(err => {
+        console.error("Error fetching build number:", err);
+        console.log("number=1"); // 如果出错，返回默认值 1
+    });
