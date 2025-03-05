fe226d8a27e8d604741f8263f77a80f9614e0bcd
hotianbexuanto <1772720865@qq.com>
2025-03-05 23:46:56 +0800

    修复 Lithium 混入兼容性问题
    - 更新 Lithium 包导入路径
    - 调整 ChunkAwareBlockCollisionSweeperMixin 注解方法签名
    - 简化 LithiumEntityCollisionsMixin 代码结构

--- src/main/java/meteordevelopment/meteorclient/mixin/lithium/LithiumEntityCollisionsMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/lithium/LithiumEntityCollisionsMixin.java
@@ -7,7 +7,8 @@ package meteordevelopment.meteorclient.mixin.lithium;
 
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.world.Collisions;
-import net.caffeinemc.mods.lithium.common.entity.LithiumEntityCollisions;
+import me.jellysquid.mods.lithium.common.entity.LithiumEntityCollisions;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.border.WorldBorder;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,11 +17,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = LithiumEntityCollisions.class)
-public abstract class LithiumEntityCollisionsMixin {
+public class LithiumEntityCollisionsMixin {
     @Inject(method = "isWithinWorldBorder", at = @At("HEAD"), cancellable = true)
-    private static void onIsWithinWorldBorder(WorldBorder border, Box box, CallbackInfoReturnable<Boolean> cir) {
-        if (Modules.get().get(Collisions.class).ignoreBorder()) {
-            cir.setReturnValue(true);
-        }
+    private static void onIsWithinWorldBorder(WorldBorder border, Box box, CallbackInfoReturnable<Boolean> info) {
+        if (Modules.get().get(Collisions.class).ignoreBorder()) info.setReturnValue(true);
     }
 }
